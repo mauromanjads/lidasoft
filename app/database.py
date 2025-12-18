66e1f19b6f656597176353ee8ff91b067447519a
MauricioManjarres <mauricio.manjarresd@pca.edu.co>
2025-12-18 07:00:41 -0500

    volvemos la bd a la nube

--- app/database.py
+++ app/database.py
@@ -4,18 +4,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # DATOS DE CONEXIÓN (LOCAL)
-POSTGRES_USER = "postgres"      # o el que creaste
-POSTGRES_PASSWORD = "sa"
-POSTGRES_DB = "lidasoftDic2025"        # igual al que migraste
-POSTGRES_HOST = "localhost"     # o IP si está en red o nube
-POSTGRES_PORT = "5432"          # puerto por defecto
+#POSTGRES_USER = "postgres"      # o el que creaste
+#POSTGRES_PASSWORD = "sa"
+#POSTGRES_DB = "lidasoftDic2025"        # igual al que migraste
+#POSTGRES_HOST = "localhost"     # o IP si está en red o nube
+#POSTGRES_PORT = "5432"          # puerto por defecto
 
 # DATOS DE CONEXIÓN (NUBE)
-#POSTGRES_USER = "neondb_owner"      # o el que creaste
-#POSTGRES_PASSWORD = "npg_dZJI8k3bzalf"
-#POSTGRES_DB = "lidasoftnube"        # igual al que migraste
-#POSTGRES_HOST = "ep-raspy-violet-a8q3d64u.eastus2.azure.neon.tech"     # o IP si está en red o nube
-#POSTGRES_PORT = "5432"          # puerto por defecto
+POSTGRES_USER = "neondb_owner"      # o el que creaste
+POSTGRES_PASSWORD = "npg_dZJI8k3bzalf"
+POSTGRES_DB = "lidasoftnube"        # igual al que migraste
+POSTGRES_HOST = "ep-raspy-violet-a8q3d64u.eastus2.azure.neon.tech"     # o IP si está en red o nube
+POSTGRES_PORT = "5432"          # puerto por defecto
 
 
 
