6c2bd954501bbed099073c2fce165ddefa1fb854
MauricioManjarres <mauricio.manjarresd@pca.edu.co>
2026-01-06 16:42:43 -0500

    movimentos page

--- app/dashboard/movimientos/page.tsx
+++ app/dashboard/movimientos/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import MovimientoFormComponent from "@/components/forms/MovimientoForm";
 
-const FacturasPage: React.FC = () => {
+const MovimientosPage: React.FC = () => {
   return (    
       <MovimientoFormComponent />    
   );
 };
 
-export default FacturasPage;
+export default MovimientosPage;
    
